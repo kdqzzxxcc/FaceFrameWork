0fc186c6abbed1b35b85fd64ead57bc575dbb031
kdq <375143304@qq.com>
2015-10-12 21:22:24 +0800

    Add note for code

--- MainWindows.py
+++ MainWindows.py
@@ -17,12 +17,26 @@ class MainWindow(QtGui.QMainWindow):
     def init(self):
         self.resize(1280, 960)
         self.setWindowTitle('FaceFrameWork')
+
+        # set split layout
+
         self.split = QtGui.QSplitter(Qt.Horizontal, self)
+
+        # left widget is camera widget, this widget deal with take photos through OpenCV Lib
+        # ans transfer OpenCV Pic to PyQt4 QImage, then use QPainter to show PyQt Pic
+
         self.left = CameraWidget(self.split)
         self.split.setStretchFactor(0, 1)
+
+        # right widget is display widget, this widget deal with process face detection algorithm,
+        # and show result in it
+
         self.right = Display(self.split)
         self.split.setStretchFactor(1, 3)
+        
         self.setCentralWidget(self.split)
+
+        # this is menu bar, in this instance, just use one to load pic
 
         menu = self.menuBar()
         file = menu.addMenu('File')
@@ -40,7 +54,9 @@ class MainWindow(QtGui.QMainWindow):
         file_name = unicode(QtGui.QFileDialog().getOpenFileName(self,'Open', self.tr(''), self.tr('*')))
         if len(file_name) == 0:
             return False
-        ### TODO READ PIC IN CAMERAWIDGET
+        ### TODO READ PIC IN DISPLAY WIDGET
+        # Show pic in right widget and process face detection algorithm
+
 
 
 
